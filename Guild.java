10762a07f2cf7dca24a870e473eb1274d6ddd49e
Guigui031 <guillaumegenois@yahoo.ca>
2023-02-28 12:10:44 -0500

    Update Guild.java

--- Guild.java
+++ Guild.java
@@ -1,7 +1,27 @@
 public class Guild {
-    public Guild() {
-        System.out.println("hello world");
-        System.out.println("test");
-        System.out.println("Hello");
+    private Hero[] heroes;  // à modifier si nécessaire, juste en attendant
+    private String[] errors;  // idem, utiliser Arrays?
+    public Guild(initialMoney, initialArmor) {
+
+    }
+
+    public void buyHero(String name, int level, double moneyCost, int armorCost, double hitPoints) {
+
+    }
+
+    public void trainHero(String name) {
+        if (!heroExist(name)) {
+            String error = "-Le héros au nom de" + name + "n'apparêt pas dans la liste\n";
+            // TODO: ajouter error à errors
+        }
+    }
+
+    private boolean heroExist(String name) {
+        for (Hero hero : heroes) {
+            if (hero.getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
