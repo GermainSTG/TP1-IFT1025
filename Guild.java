10439f0c3a06007e3a23ad143b52a03c50bf3f28
Guigui031 <guillaumegenois@yahoo.ca>
2023-02-28 13:44:34 -0500

    Update Guild.java

--- Guild.java
+++ Guild.java
@@ -8,7 +8,27 @@ public class Guild {
     }
 
     public void buyHero(String name, int level, double moneyCost, int armorCost, double hitPoints) {
-
+        if (moneyCost >= bank.getMoney() and armorCost >= bank.getArmor()) {
+            Hero newHero;
+            if (level == 0) {
+                newHero = new Common(name, hitPoints);
+            } else if (level == 1) {
+                newHero = new Uncommon(name, hitPoints);
+            } else if(level == 2) {
+                newHero = new Rare(name, hitPoints);
+            } else if (level == 3) {
+                newHero = new Epic(name, hitPoints);
+            } else if (level == 4) {
+                newHero = new Legendary(name, hitPoints);
+            } else {
+                String error = "-Ce type de Héros n'existe pas"
+                // TODO: ajouter error à errors
+            }
+            // TODO: ajouter newHero à heroes
+        } else {
+            String error = "-Il vous manque de l'argent et/ou des armures pour acheter" + name +"\n";
+            // TODO: ajouter error à errors
+        }
     }
 
     public void trainHero(String name) {
@@ -17,7 +37,7 @@ public class Guild {
                 double c = Math.log(hero.getLevel() + 10);  // une constante reveant dans les calculs de prix
                 double moneyCost = 20 * c;
                 double armorCost = Math.roof(c);
-                if ((moneyCost >= bank.getMoney()) and (armorCost > bank.getArmor())) {
+                if ((moneyCost >= bank.getMoney()) and (armorCost >= bank.getArmor())) {
                     bank.loseMoney(moneyCost);
                     bank.loseArmor(armorCost);
                     hero.upgrade();
