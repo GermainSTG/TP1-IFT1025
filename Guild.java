ad9379cfbfdc230a16dd2921a1125a32b4c58ba1
Guillaume <guillaumegenois@yahoo.ca>
2023-03-01 11:41:25 -0500

    peaufinage

--- Guild.java
+++ Guild.java
@@ -3,7 +3,7 @@ public class Guild {
 
     private Hero[] heroes;  // à modifier si nécessaire, juste en attendant
     private String[] errors;  // idem, utiliser Arrays?
-    private Bank bank;
+    private final Bank bank;
 
     public Guild(double initialMoney, int initialArmor) {
         this.bank = new Bank(initialMoney, initialArmor);
@@ -15,17 +15,13 @@ public class Guild {
             switch (level) {
                 case 0 -> {
                     newHero = new CommonHero(name, hitPoints);
-                }
-                case 1 -> {
+                } case 1 -> {
                     newHero = new UncommonHero(name, hitPoints);
-                }
-                case 2 -> {
+                } case 2 -> {
                     newHero = new RareHero(name, hitPoints);
-                }
-                case 3 -> {
+                } case 3 -> {
                     newHero = new EpicHero(name, hitPoints);
-                }
-                case 4 -> {
+                } case 4 -> {
                     newHero = new LegendaryHero(name, hitPoints);
                 }
             }
@@ -39,10 +35,10 @@ public class Guild {
     public void trainHero(String name) {
         for (Hero hero : heroes) {
             if (hero.getName().equals(name)) {  // trouve le héros à upgrade
-                double c = Math.log(hero.getLevel() + 10);  // une constante reveant dans les calculs de prix
+                double c = Math.log(hero.getLevel() + 10);  // une constante revenant dans les calculs de prix
                 double moneyCost = 20 * c;
                 int armorCost = (int) Math.floor(c);
-                if (moneyCost >= bank.getMoney() & armorCost >= bank.getArmor()) {
+                if (bank.getMoney() >= moneyCost & bank.getArmor() >= armorCost) {
                     bank.loseMoney(moneyCost);
                     bank.loseArmor(armorCost);
                     hero.upgrade();
@@ -50,10 +46,11 @@ public class Guild {
                     String error = "-Il vous manque de l'argent et/ou des armures pour améliorer" + name +"\n";
                     // TODO: ajouter error à errors
                 }
-                return;  // non vu que void?, à modifier pour que arrête là
+                return;
             }
         }
-        String error = "-Le héros au nom de" + name + "n'apparêt pas dans la liste\n";
+        // si n'a pas trouvé le héro
+        String error = "-Le héros au nom de" + name + "n'apparait pas dans la liste\n";
         // TODO: ajouter error à errors
     }
 
