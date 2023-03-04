daf8a64549f4736a384c82d9c1ffff90f15f6091
Germain STANG <46297837+GermainSTG@users.noreply.github.com>
2023-03-04 22:46:06 +0100

    Update LevelSort.java

--- LevelSort.java
+++ LevelSort.java
@@ -1,6 +1,6 @@
 import java.util.Comparator;
 
-import heros.*;
+import heroes.*;
 
 /* La classe LevelSort implemente la class comparator et créée la methode 
  compare qui permettra de trier une liste de heroes dans l'ordre croissant 
